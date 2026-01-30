4f3338c7c8dfc98db79c28f781797ac7da193943
tuomaseerola <tuomas.eerola@btinternet.com>
2026-01-30 14:24:04 +0000

    Minor relabelling of the sub-constructs take 2

--- exp2/scr/rename_specific_items.R
+++ exp2/scr/rename_specific_items.R
@@ -2,18 +2,19 @@ rename_specific_items <- function(data = NULL){
   if(is.null(data)){
     stop("Data is NULL. Please provide a data frame.")
   }
-
+  # print("Exp. 2 renaming items")
   # Rename  sub-constructs:
 
   # Aesthetic-Interest-Awe
   # B "Being moved/spirituality" (was I)
   # I "Curiosity/Interest" (was X)
   # A "Aesthetics" (was B)
-  data$item_label <- str_replace_all(data$item_label,
-                                     c("B" = "A",
-                                       "I" = "B",
-                                       "X" = "I"
-                                       ))
+#  data$item_label <- str_replace_all(data$item_label,
+#                                     c("B" = "A",
+#                                       "I" = "B",
+#                                       "X" = "I"
+#                                       ))
+
 
   #   Connection-Belonging
   # NEW								OLD
@@ -22,6 +23,19 @@ rename_specific_items <- function(data = NULL){
   data$item_label <- str_replace_all(data$item_label,
                                     c("C" = "G",
                                       "S" = "L"))
+  
+  # Aesthetic-Interest-Awe
+  # S "Being moved/spirituality" (was I)
+  # I "Curiosity/Interest" (was X)
+  # B "Aesthetics/Beauty" (was B)
+
+
+  data$item_label <- str_replace_all(data$item_label,
+                                     c("B" = "B",
+                                       "I" = "S",
+                                       "X" = "I"
+                                       ))
+
   # Personal-Emotional-Processing
   #
   # C "Reflection/Coping" (was U)
